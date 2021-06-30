867771690bd3ac43d1479542445931fe9404a6b6
Hancel.Lin <imlinhanchao@gmail.com>
2021-06-30 23:00:23 +0800

    add feature to send hex and file

--- src/comment.js
+++ src/comment.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode');
 const locale = require('../i18n')();
+const fs = require('fs');
 
 let attrOptions = {
 	baudRate: [115200, 57600, 38400, 19200, 9600, 4800, 2400, 1800, 1200, 600, 300, 200, 150, 134, 110, 75, 50],
@@ -7,6 +8,38 @@ let attrOptions = {
 	stopBits: [1, 2],
 	parity: ['none', 'even', 'mark', 'odd', 'space']
 };
+
+async function getSendData(data, path) {
+	if (!data) return data;
+	// send file
+	if (data.startsWith('@file')) {
+		let filepath = data.replace(/^@file:/, '');
+		if(!fs.existsSync(filepath)) {
+			filepath = await vscode.window.showOpenDialog({ canSelectMany: false });
+			if (filepath.length == 0) return null;
+			filepath = filepath[0].fsPath;
+		}
+
+		return fs.readFileSync(filepath);
+	}
+
+	// send hex
+	if (data.startsWith('@hex')) {
+		data = data.replace(/^@hex:/, '');
+		if (data.match(/^\s*([0-9A-Fa-f]{2}\s+)*[0-9A-Fa-f]{2}\s*$/g) == null) {
+			data = await vscode.window.showInputBox({ prompt: locale['send_hex_title'].replace(/{{path}}/, path) });
+			if (!data) return data;
+			if (data.match(/^\s*([0-9A-Fa-f]{2}\s+)*[0-9A-Fa-f]{2}\s*$/g) == null) {
+				vscode.window.showErrorMessage(locale['not_invalid_hex']);
+				return null;
+			}
+		}
+		data = data.match(/[0-9A-Fa-f]{2}\s*/g).map(d => parseInt(d.trim(), 16));
+		return Buffer.from(data);
+	}
+
+	return data;
+}
 
 module.exports = {
     async connectOrDisconect(port) {
@@ -31,9 +64,10 @@ module.exports = {
 	},
 
 	async sendEntry(port) {
-		let data = await vscode.window.showInputBox({ title: locale['send_title'].replace(/{{path}}/, port.path) });
+		let data = await vscode.window.showInputBox({ prompt: locale['send_title'].replace(/{{path}}/, port.path) });
+		data = await getSendData(data, port.path);
 		if (!data) return;
-		let ret = await port.port.send(data + '\n');
+		let ret = await port.port.send(data);
 		if (ret) vscode.window.showInformationMessage(locale['send_success'].replace(/{{path}}/, port.path));
 		else vscode.window.showErrorMessage(`Send Data to ${port.path} was failed. Error Message: ${port.lasterror}`);
 	}
